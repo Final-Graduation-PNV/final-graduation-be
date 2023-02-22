92e313a55952d4fc7cd68b39d65ca941055d6f2b
DThanh1508 <athanh2002kt@gmail.com>
2023-02-22 14:54:04 +0700

    feature/thanh/PFA-138: Make category and role seeder

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $category = ['plant', 'flower', 'indoor plant', 'outdoor plant', 'indoor flower', 'outdoor flower'];
+        for ($i = 0; $i<6; $i++) {
+            DB::table('categories')->insert([
+                'name' => $category[$i],
+            ]);
+        }
+    }
+}
