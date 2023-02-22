abd43ae5c4b345036cee68943299fea9c341142c
DThanh1508 <athanh2002kt@gmail.com>
2023-02-22 22:52:09 +0700

    feature/thanh/PFA-133: Finished login, resgister and logout function

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(RegisterRequest $request){
+        $validation  = $request->validated();
+        $user = User::create([
+            'name'     => $validation['name'],
+            'email'    => $validation['email'],
+            'password' => bcrypt($validation['password']),
+            'address'  => $validation['address'],
+            'city'     => $validation['city']
+        ]);
+
+        DB::table('role_user')->insert([
+            'role_id' => 3,
+            'user_id' => $user->id
+        ]);
+
+        $token = $user->createToken('apiToken', ['server:update'])->plainTextToken;
+
+        $res = [
+            'user' => $user,
+            'token' => $token
+        ];
+        return response($res, 201);
+    }
+
+    public function login(Request $request)
+    {
+        $data = $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string'
+        ]);
+
+        $user = User::where('email', $data['email'])->first();
+
+        if (!$user || !Hash::check($data['password'], $user->password)) {
+            return response([
+                'msg' => 'Incorrect email or password'
+            ], 400);
+        }
+
+        $token = $user->createToken('apiToken')->plainTextToken;
+
+        $res = [
+            'user' => $user->name,
+            'token' => $token,
+            'message' => 'Logged successfully'
+        ];
+
+        return response($res, 200);
+    }
+
+    public function logout(Request $request)
+    {
+        auth()->user()->tokens()->delete();
+        return [
+            'message' => 'user logged out'
+        ];
+    }
+}
