6bf23b7e50f9db35d7d2709a029b10f5a021ccb5
DThanh1508 <athanh2002kt@gmail.com>
2023-02-28 08:24:12 +0700

    feature/thanh/PFA-138: edit type price in product table

--- database/migrations/2023_02_21_152904_create_products_table.php
+++ database/migrations/2023_02_21_152904_create_products_table.php
@@ -17,7 +17,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->decimal('price');
+            $table->decimal('price',25,2);
             $table->text('description');
             $table->text('image');
             $table->integer('quantity');
