c8367dde5de10f55702f07cbf7338be7157b4ec7
DThanh1508 <athanh2002kt@gmail.com>
2023-03-18 16:28:16 +0700

    feature/thanh/PFA-302: Route for vnpay payment and return status

--- routes/api.php
+++ routes/api.php
@@ -85,6 +85,8 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
         Route::delete('/shop/products/{id}', [ProductController::class, 'destroy']);
         Route::get('/shop/check', [ShopOwnerController::class, 'checkoutAccount']);
         Route::get('/shop/vnpay/create', [ShopOwnerController::class, 'checkoutPayMent']);
+        Route::get('/shop/vnpay/payment', [ShopOwnerController::class, 'vnpayPayment']);
+        Route::get('/shop/vnpay/return', [ShopOwnerController::class, 'vnpayReturn']);
     });
 
     /**
