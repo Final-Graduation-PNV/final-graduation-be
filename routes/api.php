9c84230f03ff794adb0b23059013ebc46584f492
DThanh1508 <athanh2002kt@gmail.com>
2023-03-19 16:46:09 +0700

    feature/thanh/PFA-302: Fix route payment

--- routes/api.php
+++ routes/api.php
@@ -38,6 +38,7 @@ Route::post('/email/verify-otp/{id}', [VerificationController::class, 'verifyOTP
 Route::post('/email/logout-otp/{id}', [VerificationController::class, 'destroy']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/categories', [AuthController::class, 'category']);
+Route::get('/shop/vnpay/payment', [ShopOwnerController::class, 'vnpayPayment'])->name('return');
 
 /**
  * Private authors routes.
@@ -85,7 +86,6 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
         Route::delete('/shop/products/{id}', [ProductController::class, 'destroy']);
         Route::get('/shop/check', [ShopOwnerController::class, 'checkoutAccount']);
         Route::get('/shop/vnpay/create', [ShopOwnerController::class, 'checkoutPayMent']);
-        Route::get('/shop/vnpay/payment', [ShopOwnerController::class, 'vnpayPayment'])->name('return');
         Route::get('/shop/vnpay/return', [ShopOwnerController::class, 'vnpayReturn']);
     });
 
