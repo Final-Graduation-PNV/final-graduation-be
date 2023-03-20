8ffd164b4d5f03c4b2998466e75e90b04f1e8e5b
DThanh1508 <athanh2002kt@gmail.com>
2023-03-20 15:01:25 +0700

    feature/thanh/PFA-272: Delete auth controller file

--- app/Http/Controllers/API/Authentication/AuthController.php
+++ app/Http/Controllers/API/Authentication/AuthController.php
@@ -1,11 +1,10 @@
 <?php
 
-namespace App\Http\Controllers\API;
+namespace App\Http\Controllers\API\Authentication;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\RegisterRequest;
 use App\Mail\UserVerification;
-use App\Models\Category;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
@@ -97,6 +96,58 @@ class AuthController extends Controller
         ], 500);
     }
 
+    public function verifyOTP(Request $request, $id)
+    {
+        $data = $request->validate([
+            'otp' => 'required|integer|min:100000'
+        ]);
+
+        $user = User::findOrFail($id);
+
+        if (!$user) {
+            return response()->json(["message" => "User does not exist"], 400);
+        } elseif (Carbon::now()->gt($user->confirmation_code_expired_in)) {
+            return response()->json(["message" => "Your OTP expired"], 400);
+        } else {
+            if ($data['otp'] != $user->confirmation_code) {
+                return response()->json(["message" => "Your OTP is invalid"], 400);
+            }
+
+            $user->email_verified = true;
+            $user->save();
+
+            DB::table('role_user')->insert([
+                'role_id' => 3,
+                'user_id' => $user->id
+            ]);
+
+            $token = $user->createToken('apiToken', ['server:update'])->plainTextToken;
+
+            $res = [
+                'message' => "Successfully verified",
+                'user' => $user,
+                'token' => $token
+            ];
+
+            return response()->json($res, 201);
+        }
+    }
+
+    public function cancel($id)
+    {
+        $user = User::find($id);
+
+        if ($user->email_verified == false) {
+            $result = $user->delete();
+            if ($result)
+                return response()->json(["message" => "Canceled account registration!"], 200);
+            else {
+                return response()->json(["message" => "Account deletion failed!"], 400);
+            }
+        }
+        return response()->json(["message" => "Unauthorized"], 400);
+    }
+
     public function login(Request $request)
     {
         $data = $request->validate([
@@ -146,15 +197,5 @@ class AuthController extends Controller
         return [
             'message' => 'user logged out'
         ];
-    }
-
-    public function category()
-    {
-        $categories = Category::select('id', 'name')
-            ->get();
-
-        return response()->json([
-            'categories' => $categories,
-        ], 200);
     }
 }
