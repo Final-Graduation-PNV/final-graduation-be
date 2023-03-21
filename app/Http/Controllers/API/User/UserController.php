6cbf6872f93285dc02ba86bf30c3d5b494e18451
DThanh1508 <athanh2002kt@gmail.com>
2023-03-21 23:53:40 +0700

    feature/thanh/PFA-251: Finished show profile and edit profile for user

--- app/Http/Controllers/API/User/UserController.php
+++ app/Http/Controllers/API/User/UserController.php
@@ -4,12 +4,57 @@ namespace App\Http\Controllers\API\User;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
+    public function profile(Request $request)
+    {
+        try {
+            $id = $request->user()->id;
+
+            $user = User::where('id', $id)->first(['id', 'name', 'email', 'avatar', 'phone', 'birth', 'address', 'city', 'renewal']);
+
+            return response()->json([
+                'user' => $user
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json(['message' => 'An error occurred while fetching user data'], 500);
+        }
+    }
+
+    public function editProfile(Request $request)
+    {
+        try {
+            $id = $request->user()->id;
+            $user = User::find($id);
+
+            // If user not found, return error
+            if (!$user) {
+                return response()->json(['message' => 'User not found'], 404);
+            }
+
+            $data = $request->only(['name', 'avatar', 'phone', 'birth', 'gender', 'address', 'city']);
+
+            // If no data was submitted, return success message
+            if (empty(array_filter($data))) {
+                return response()->json(['message' => 'Information has not changed!'], 200);
+            }
+
+            // Update user's profile
+            $user->fill($data);
+            $user->save();
+
+            return response()->json(['message' => 'Profile updated successfully!'], 200);
+        } catch (Exception $e) {
+            return response()->json(['message' => 'An error occurred while updating user profile'], 500);
+        }
+    }
+
     public function beShopOwner(Request $request)
     {
         $id = $request->user()->id;
