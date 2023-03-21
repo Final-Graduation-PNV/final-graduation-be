200de6e988562261dc0021c0feeb7aae96559a18
DThanh1508 <athanh2002kt@gmail.com>
2023-03-21 15:40:30 +0700

    feature/thanh/PFA-304: Added end time column

--- vendor/laravel/framework/src/Illuminate/Database/Schema/Blueprint.php
+++ vendor/laravel/framework/src/Illuminate/Database/Schema/Blueprint.php
@@ -1175,6 +1175,8 @@ class Blueprint
         $this->timestamp('created_at', $precision)->nullable();
 
         $this->timestamp('updated_at', $precision)->nullable();
+
+        $this->timestamp('end_time', $precision)->nullable();
     }
 
     /**
