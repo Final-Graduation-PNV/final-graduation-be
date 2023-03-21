e7247aaa9af6cb5914bbfb315831aa98e2cc5e90
DThanh1508 <athanh2002kt@gmail.com>
2023-03-21 22:22:12 +0700

    fix/thanh/PFA-272: Edit checkoutAccount function

--- app/Http/Controllers/API/ShopOwner/ShopOwnerController.php
+++ app/Http/Controllers/API/ShopOwner/ShopOwnerController.php
@@ -7,56 +7,59 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class ShopOwnerController extends Controller
 {
     public function checkoutAccount(Request $request)
     {
-        $id = $request->user()->id;
+        try {
+            $id = $request->user()->id;
 
-        $shop = User::join('role_user', 'role_user.user_id', '=', 'users.id')
-            ->where('role_user.user_id', $id)
-            ->first('users.*');
+            $shop = User::join('role_user', 'role_user.user_id', '=', 'users.id')
+                ->where('role_user.user_id', $id)
+                ->first('users.*');
 
-        if ($shop && $shop->end_time) {
-            $date = Carbon::createFromFormat('Y-m-d H:i:s', $shop->end_time)->format('Y-m-d');
-            $expires = Carbon::now()->format('Y-m-d');
+            if ($shop && $shop->end_time) {
+                $date = Carbon::createFromFormat('Y-m-d H:i:s', $shop->end_time)->format('Y-m-d');
+                $expires = Carbon::now()->format('Y-m-d');
 
-            if ($date === $expires) {
-                $delete = DB::table('role_user')
-                    ->where('role_id', 2)
-                    ->where('user_id', $id)
-                    ->delete();
-                $expireArray = [
-                    'message' => 'Your account has expired. You must pay to continue using!',
-                    'name' => $shop->name,
-                    'date_used' => $date,
-                    'date_expires' => $expires
-                ];
-                if ($delete) {
+                if ($date === $expires) {
+                    $shop->renewal = false;
+                    $shop->save();
+                    $expireArray = [
+                        'message' => 'Your account has expired. You must pay to continue using!',
+                        'name' => $shop->name,
+                        'date_used' => $date,
+                        'date_expires' => $expires
+                    ];
                     return response()->json([
                         'valid_account' => [$expireArray]
                     ], 402);
                 } else {
+                    $validArray = [
+                        'message' => 'Your account has not expired!',
+                        'name' => $shop->name,
+                        'date_used' => $date,
+                        'date_expires' => $expires
+                    ];
+
                     return response()->json([
-                        'valid_account' => [$expireArray]
-                    ], 402);
+                        'valid_account' => [$validArray]
+                    ], 200);
                 }
             } else {
-                $validArray = [
-                    'message' => 'Your account has not expired!',
-                    'name' => $shop->name,
-                    'date_used' => $date,
-                    'date_expires' => $expires
-                ];
-
                 return response()->json([
-                    'valid_account' => [$validArray]
-                ], 200);
+                    'message' => 'Something went wrong!',
+                ], 500);
             }
-        } else {
+        } catch (\Exception $e) {
+            // Log the error for debugging purposes
+            Log::error($e);
+
+            // Return an error response to the client
             return response()->json([
-                'message' => 'Something went wrong!',
+                'message' => 'An error occurred while processing your request.',
             ], 500);
         }
     }
