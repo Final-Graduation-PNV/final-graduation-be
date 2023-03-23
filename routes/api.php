73e391d0fc8d7b7c04a95072a706dc1c01280e37
DThanh1508 <athanh2002kt@gmail.com>
2023-03-23 08:50:25 +0700

    feature/thanh/PFA-201: Edit route

--- routes/api.php
+++ routes/api.php
@@ -38,7 +38,7 @@ Route::controller(AuthController::class)->group(function () {
     Route::post('/users/resend-otp','resendOTP');
     Route::post('/users/{id}','cancel');
     Route::patch('/users/forgot','forgotPassword');
-    Route::get('/users/{id}/forgot','verifyOTP');
+    Route::post('/users/{id}/forgot','verifyOTP');
     Route::post('/users/{id}/password','changePassword');
     Route::post('/login','login');
 });
