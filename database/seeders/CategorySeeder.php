d2c76f012986c8c0a36ec2e292181cebeffaf818
DThanh1508 <athanh2002kt@gmail.com>
2023-03-24 02:23:04 +0700

    fix/thanh/PFA-272: End last sprint!!!

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -14,7 +14,7 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        $category = ['plant', 'flower', 'indoor plant', 'outdoor plant', 'indoor flower', 'outdoor flower'];
+        $category = ['Plant', 'Flower', 'Indoor plant', 'Outdoor plant', 'Indoor flower', 'Outdoor flower'];
         for ($i = 0; $i < 6; $i++) {
             DB::table('categories')->insert([
                 'name' => $category[$i],
